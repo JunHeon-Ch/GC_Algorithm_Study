0468ef746e443400b11207a6df39f4f4785c4ac1
BlackChang <dnjs7651@gmail.com>
2020-12-30 23:05:01 +0900

    Success

--- wonchang/기초1_2_수학1/2089/2089/main.cpp
+++ wonchang/기초1_2_수학1/2089/2089/main.cpp
@@ -6,9 +6,29 @@
 //
 
 #include <iostream>
-
+#include <cstring>
+#include <algorithm>
+using namespace std;
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    long n = 0;
+    string ans = "";
+    cin >> n;
+    if(n == 0){
+        cout << "0\n";
+        return 0;
+    }
+    while(n != 0){
+        if(n % -2 == 0){
+            ans += '0';
+            n /= -2;
+        }
+        else{
+            ans += '1';
+            n = (n - 1) / -2;
+        }
+    }
+    
+    reverse(ans.begin(), ans.end());
+    cout << ans << endl;
     return 0;
 }
