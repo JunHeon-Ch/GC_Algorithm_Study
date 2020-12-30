3bd0c93fdd3bc202ae46319eca0a431d27e817bd
BlackChang <dnjs7651@gmail.com>
2020-12-30 19:25:07 +0900

    New Project

---
+++ wonchang/기초1_2_수학1/2089/2089/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  2089
+//
+//  Created by 이원창 on 2020/12/30.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
