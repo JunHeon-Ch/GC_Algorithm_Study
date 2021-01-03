291a549f0829375ca6b3fbbb06a82a1f8bf43bb2
JunHeon-Ch <51908258+JunHeon-Ch@users.noreply.github.com>
2021-01-03 21:47:04 +0900

    17103 골드바흐 파티션

--- junheon/기초/수학/17103 골드바흐 파티션/17103.cpp
+++ junheon/기초/수학/17103 골드바흐 파티션/17103.cpp
@@ -38,46 +38,6 @@ int main() {
         }
         cout << cnt << '\n';
     }
-    cout << prime.size();
 
     return 0;
 }
-
-// #include <iostream>
-// #include <vector>
-// using namespace std;
-// bool check[1000001];
-// int main() {
-//     ios_base::sync_with_stdio(false);
-//     cin.tie(nullptr);
-//     vector<int> primes;
-//     for (int i=2; i<=1000000; i++) {
-//         if (check[i] == false) {
-//             primes.push_back(i);
-//             for (int j=i+i; j<=1000000; j+=i) {
-//                 check[j] = true;
-//             }
-//         }
-//     }
-//     int t;
-//     cin >> t;
-//     while (t--) {
-//         int n;
-//         cin >> n;
-//         int ans = 0;
-//         for (int i=0; i<primes.size(); i++) {
-//             if (n-primes[i] >= 2 && primes[i] <= n-primes[i]) {
-//                 if (check[n-primes[i]] == false) {
-//                     ans += 1;
-//                 }
-//             } else {
-//                 break;
-//             }
-//         }
-//         cout << ans << '\n';
-//     }
-
-//     cout << primes.size();
-
-//     return 0;
-// }
