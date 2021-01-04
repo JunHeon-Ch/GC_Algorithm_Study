c5c74e9ed2a18ade1c5a8a9dd20d4717c78c7850
JunHeon-Ch <51908258+JunHeon-Ch@users.noreply.github.com>
2021-01-04 11:28:46 +0900

    2745 진법 변환

--- junheon/기초/수학/2745 진법 변환/2745.cpp
+++ junheon/기초/수학/2745 진법 변환/2745.cpp
@@ -8,15 +8,14 @@ int main() {
     int b;
     cin >> n >> b;
 
-    int ans = 0, exp = 1;
+    int ans = 0;
     for(int i = n.length() - 1; i >= 0; i--) {
         if(n[i] - '0' >= 0 && n[i] - '0' <= 9) {
-            ans += (n[i] - '0') * exp;
+            ans = ans * b + (n[i] - '0');
         }
         else if(n[i] - 'A' >= 0 && n[i] - 'A' <= 26) {
-            ans += (n[i] - 'A' + 10) * exp;
+            ans = ans * b + (n[i] - 'A' + 10);
         }
-        exp *= b;
     }
     cout << ans;
 
