b4f3745e9ee849cd7951950dbf5dbb032b86867b
JunHeon-Ch <chjh121@gmail.com>
2021-01-05 13:52:55 +0900

    2004 조합 0의 개수

--- junheon/기초/수학/#9_2004 조합 0의 개수/2004.cpp
+++ junheon/기초/수학/#9_2004 조합 0의 개수/2004.cpp
@@ -10,16 +10,24 @@ int main() {
     int cnt5 = 0, cnt2 = 0;
     for(long long i = 5; i <= n; i *= 5) {
         cnt5 += n / i;
+    }
+    for(long long i = 5; i <= m; i *= 5) {
         cnt5 -= m / i;
+    }
+    for(long long i = 5; i <= n - m; i *= 5) {
         cnt5 -= (n - m) / i;
     }
-    for(long long i = 2; i <= n; i *= 2) {
+    for(long long i = 5; i <= n; i *= 5) {
         cnt2 += n / i;
+    }
+    for(long long i = 5; i <= m; i *= 5) {
         cnt2 -= m / i;
+    }
+    for(long long i = 5; i <= n - m; i *= 5) {
         cnt2 -= (n - m) / i;
     }
-    cout << min(cnt2, cnt5) << '\n';
 
+    cout << min(cnt2, cnt5) << '\n';
 
     return 0;
 }
