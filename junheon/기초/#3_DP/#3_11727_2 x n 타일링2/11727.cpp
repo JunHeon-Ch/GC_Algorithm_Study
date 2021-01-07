7115e57393e0c522daa827f293cf691b9b824d9f
JunHeon-Ch <chjh121@gmail.com>
2021-01-07 14:37:08 +0900

    11727 2 x n 타일링2

---
+++ junheon/기초/#3_DP/#3_11727_2 x n 타일링2/11727.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+int cases[1001];
+const int DIV = 10007;
+
+int main() {
+    int n;
+    cin >> n;
+
+    cases[1] = 1;
+    cases[2] = 3;
+    for(int i = 3; i <= n; i++) {
+        cases[i] = (cases[i - 1] + 2 * cases[i - 2]) % DIV;
+    }
+    cout << cases[n] << '\n';
+
+    return 0;
+}
