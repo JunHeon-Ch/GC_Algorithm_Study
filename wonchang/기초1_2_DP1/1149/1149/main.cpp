bbaa299cd54a88e65b0b4039f510a8e5268d1b4f
BlackChang <dnjs7651@gmail.com>
2021-01-08 18:31:07 +0900

    Success

--- wonchang/기초1_2_DP1/1149/1149/main.cpp
+++ wonchang/기초1_2_DP1/1149/1149/main.cpp
@@ -6,15 +6,31 @@
 //
 
 #include <iostream>
+#include <climits>
 using namespace std;
 int main(int argc, const char * argv[]) {
     int n = 0;
+    int ans = INT_MAX;
     int rgb[1001][3];
+    int dp [1001][3];
     
     cin >> n;
     for(int i = 1; i <= n; i++){
-        cin >> rbg[i][0] >> rgb[i][1] >> rgb[i][2];
+        cin >> rgb[i][0] >> rgb[i][1] >> rgb[i][2];
     }
     
+    dp[1][0] = rgb[1][0];
+    dp[1][1] = rgb[1][1];
+    dp[1][2] = rgb[1][2];
+    for(int i = 2; i <= n; i++){
+        dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + rgb[i][0];
+        dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + rgb[i][1];
+        dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + rgb[i][2];
+    }
+    
+    for(int i = 0; i < 3; i++){
+        ans = min(ans, dp[n][i]);
+    }
+    cout << ans << '\n';
     return 0;
 }
