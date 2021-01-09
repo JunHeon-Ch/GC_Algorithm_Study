440c7839e04f73d9da221e0e3ab17d7ea942a094
JunHeon-Ch <chjh121@gmail.com>
2021-01-09 14:04:55 +0900

    11052 카드 구매하기

---
+++ junheon/기초/#3_DP/#5_11052 카드 구매하기/11052.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> p(n + 1);
+    vector<int> maxP(n + 1);
+    
+    for(int i = 1; i <= n; i++) {
+        cin >> p[i];
+    }
+    maxP[0] = 0;
+    for(int i = 1; i <= n; i++) {
+        maxP[i] = maxP[i - 1];
+        for(int j = 0; j < i; j++) {
+            if(maxP[i] < maxP[j] + p[i - j]) {
+                maxP[i] = maxP[j] + p[i - j];
+            }
+        }
+    }
+    cout << maxP[n] << '\n';
+
+    return 0;
+}
