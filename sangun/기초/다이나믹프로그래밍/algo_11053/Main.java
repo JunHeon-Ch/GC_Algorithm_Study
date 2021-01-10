b1781f0703c371684aa1d09b072f2051ffcf12c5
Sang-un <dltkddns79@gmail.com>
2021-01-10 20:35:15 +0900

    edit code after study

--- sangun/기초/다이나믹프로그래밍/algo_11053/Main.java
+++ sangun/기초/다이나믹프로그래밍/algo_11053/Main.java
@@ -1,49 +1,42 @@
 package algo_11053;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int[] arr = new int[1001];
         int[] dp = new int[1001];
-        int n = sc.nextInt();
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        dp[0] = 1;
 
-        // dpmax는 그 지점의 최고 길이
-        int dpmax;
-        for (int i = 0; i < n; i++) {
-            dpmax = 0;
-            for (int j = 0; j <= i; j++) {
-                // 내 지점이 전들 지점보다 크면서
-                // 지점의 최고 길이도 길거나 같다면
-                // dpmax의 길이에 그 길이를 넣어주고 그 길이에 자신을 포함하기 위해 1을 더한다.
-                if (arr[i] > arr[j] && dp[j] >= dpmax) {
-                    dpmax = dp[j];
+        for (int i = 1; i <= n; i++) {
+            dp[i] = 1;
+            for (int j = i; j >= 0; j--) {
+                if (arr[j] < arr[i] && dp[j] + 1 > dp[i]) {
                     dp[i] = dp[j] + 1;
-                }
-                // 전체를 돌았는데도 못 찾았을때는 자기자신을 1로 둠
-                if (dpmax == 0) {
-                    dp[i] = 1;
                 }
             }
         }
 
-        int max = dp[0];
-        for (int i = 0; i < n; i++) {
-            if (max < dp[i]) {
-                max = dp[i];
+        int max = 0;
+
+        for (int i : dp) {
+            if (i > max) {
+                max = i;
             }
         }
         System.out.println(max);
-
-
     }
 
 }
--- sangun/기초/다이나믹프로그래밍/algo_14002/Main.java
+++ sangun/기초/다이나믹프로그래밍/algo_14002/Main.java
@@ -1,68 +1,60 @@
 package algo_14002;
 
 import java.io.*;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb;
-        sb = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
-        int arr[] = new int[n + 1];
-        int dp[] = new int[n + 1];
-        // 값의 범위의 최솟값이 1이기 때문에
-        int result = 0;
+    static int[] arr = new int[1001];
+    static int[] dp = new int[1001];
+    static int[] v = new int[1001];
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+    public static void main(String[] args) throws IOException {
+
+
+        int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        // dp배열 초기화
+
         for (int i = 1; i <= n; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            // 입력된 배열 값 저장
-            arr[i] = num;
-            // 입력된 배열 값과 이전의 값들을 비교한다.
-            for (int j = 0; j < i; j++) {
-                // 비교한 값보다 큰경우
-                if (arr[i] > arr[j]) {
-                    // 비교한 값의 dp값 + 1, 기존의 dp값 중 큰 값을 저장한다.
-                    dp[i] = Math.max(dp[j] + 1, dp[i]);
-                    // 최장 길이인 답을 구하기 위해서 result값에
-                    // 지금까지 구한 dp값 중 가장 큰 값을 저장한다.
-                    result = Math.max(dp[i], result);
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = 1;
+            int temp = -1;
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[j] < arr[i] && dp[j] + 1 > dp[i]) {
+                    dp[i] = dp[j] + 1;
+                    temp = j;
                 }
             }
+            v[i] = temp;
+
         }
-        // 최장길이 추가
-        sb.append(result + "\n");
 
-        // 최장 길이 값
-        int value = result;
-        // 경로를 역추적 할 stack
-        Stack<Integer> stack = new Stack<>();
-
-        // value는 현재 찾는 길이에 해당하는 값이다.
-        for (int i = n; i >= 1; i--) {
-            // 현재 찾는 길이와 같은 경우
-            if (value == dp[i]) {
-                // stack에 실제 값을 push한다.
-                stack.push(arr[i]);
-                // 찾는 길이를 찾았으므로 -1을 해주어
-                // 다음에 찾을 길이를 설정해준다.
-                value--;
+        int max = 0;
+        int index = 0;
+        for (int i = 1; i <= n; i++) {
+            if (dp[i] > max) {
+                max = dp[i];
+                index = i;
             }
         }
+        v[1] = -1;
+        bw.write(max + "\n");
+        go(index);
 
-        while (!stack.isEmpty()) {
-            sb.append(stack.pop() + " ");
-        }
-
-        // 출력 버퍼에 write 작업을 한다.
-        bw.write(sb.toString());
-
+        bw.flush();
         bw.close();
-        br.close();
     }
+
+    public static void go(int p) throws IOException {
+        if (p == -1) return;
+        go(v[p]);
+        bw.write(arr[p] + " ");
+    }
+
 }
+
