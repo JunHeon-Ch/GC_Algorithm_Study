a35f98f2ba551e6f6100b32de0eae8c2c2467871
JunHeon-Ch <chjh121@gmail.com>
2021-01-17 13:20:37 +0900

    15652 N과 M(4)

---
+++ junheon/기초/#4_브루트 포스/#12_15652 N과 M(4)/15652_2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+int ans[9];
+
+void go(int index, int selected, int n, int m) {
+    if(selected == m) {
+        for(int i = 0; i < m; i++) {
+            cout << ans[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    if(index > n) return;
+    ans[selected] = index;
+    go(index, selected + 1, n, m);
+    go(index + 1, selected, n, m);
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    go(1, 0, n, m);
+    return 0;
+}
