2da525b775f97d02080cf76ae087424aded48332
BlackChang <dnjs7651@gmail.com>
2021-01-19 16:15:24 +0900

    Success

---
+++ wonchang/기초/기초2_2/기초2_2_Brute Force/15664/15664/main.cpp
@@ -0,0 +1,65 @@
+//
+//  main.cpp
+//  15664
+//
+//  Created by 이원창 on 2021/01/19.
+//
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+using namespace std;
+int ans[8];
+int arr[8];
+bool visit[8];
+vector<int> v;
+set<vector<int>> s;
+void go(int n, int m, int cnt){
+    if(cnt == m){
+        s.insert(v);
+        return;
+    }
+    if(cnt == 0){
+        for(int i = 0; i < n; i++){
+            if(!visit[i]){
+                visit[i] = true;
+                v.push_back(arr[i]);
+                go(n, m, cnt + 1);
+                visit[i] = false;
+                v.pop_back();
+            }
+        }
+    }
+    else{
+        for(int i = 0; i < n; i++){
+            if(!visit[i] && v[cnt - 1] <= arr[i]){
+                visit[i] = true;
+                v.push_back(arr[i]);
+                go(n, m, cnt + 1);
+                visit[i] = false;
+                v.pop_back();
+            }
+        }
+    }
+}
+int main(int argc, const char * argv[]) {
+    int n = 0;
+    int m = 0;
+    
+    cin >> n >> m;
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    
+    sort(arr, arr + n);
+    go(n, m, 0);
+    
+    for(auto tmp : s){
+        for(int i = 0; i < tmp.size(); i++){
+            cout << tmp[i] << ' ';
+        }
+        cout << '\n';
+    }
+    
+    return 0;
+}
