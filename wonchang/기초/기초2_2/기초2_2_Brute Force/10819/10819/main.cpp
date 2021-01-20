71a73cff01c857c59815f829cde6485b9f5ec3f3
BlackChang <dnjs7651@gmail.com>
2021-01-20 15:40:19 +0900

    Success

--- wonchang/기초/기초2_2/기초2_2_Brute Force/10819/10819/main.cpp
+++ wonchang/기초/기초2_2/기초2_2_Brute Force/10819/10819/main.cpp
@@ -8,15 +8,38 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
+bool visit[8];
+int ans = 0;
+int arr[8] = {0, };
+int temp[8] = {0, };
+void go(int n, int cnt){
+    if(cnt == n){
+        int tmp = 0;
+        for(int i = 0; i < n - 1; i++){
+            tmp += abs(temp[i] - temp[i + 1]);
+            ans = max(ans, tmp);
+        }
+    }
+    
+    for(int i = 0; i < n; i++){
+        if(!visit[i]){
+            temp[cnt] = arr[i];
+            visit[i] = true;
+            go(n, cnt + 1);
+            visit[i] = false;
+        }
+    }
+}
 int main(int argc, const char * argv[]) {
     int n = 0;
-    int arr[8];
-    
+
     cin >> n;
     for(int i = 0; i < n; i++){
         cin >> arr[i];
     }
     
+    go(n, 0);
+    cout << ans << '\n';
     
     return 0;
 }
---
+++ wonchang/기초/기초2_2/기초2_2_Brute Force/10971/10971/main.cpp
@@ -0,0 +1,49 @@
+//
+//  main.cpp
+//  10971
+//
+//  Created by 이원창 on 2021/01/20.
+//
+
+#include <iostream>
+#include <algorithm>
+#include <climits>
+using namespace std;
+int n = 0;
+int ini = 0;
+long long ans = LONG_LONG_MAX;
+long long w[10][10];
+bool visit[10];
+vector<int> v;
+void go(int start, int cnt, long long sum){
+    if(cnt == n - 1){
+        if(w[start][ini] != 0)
+            ans = min(ans, sum + w[start][ini]);
+    }
+
+    for(int i = 0; i < n; i++){
+        if(!visit[i] && w[start][i] != 0){
+            visit[i] = true;
+            go(i, cnt + 1, sum + w[start][i]);
+            visit[i] = false;
+        }
+    }
+}
+int main(int argc, const char * argv[]) {
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin >> w[i][j];
+        }
+    }
+    
+    for(int i = 0; i < n; i++){
+        ini = i;
+        visit[i] = true;
+        go(i, 0, 0);
+        visit[i] = false;
+    }
+
+    cout << ans << '\n';
+    return 0;
+}
