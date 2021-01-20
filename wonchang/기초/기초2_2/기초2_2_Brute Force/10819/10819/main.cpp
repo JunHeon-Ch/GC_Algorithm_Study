4f0bf1dc91278aeb99b6c30fa4a88d64cc2bc94d
BlackChang <dnjs7651@gmail.com>
2021-01-20 14:12:18 +0900

    Success

---
+++ wonchang/기초/기초2_2/기초2_2_Brute Force/10819/10819/main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  10819
+//
+//  Created by 이원창 on 2021/01/19.
+//
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    int n = 0;
+    int arr[8];
+    
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    
+    
+    return 0;
+}
