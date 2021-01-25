a460ac6f4d2c661a243dcb1235afdd9430951c7d
BlackChang <dnjs7651@gmail.com>
2021-01-25 19:02:54 +0900

    Success

--- wonchang/기초/기초2_2/기초2_2_Brute Force/1248/1248/main.cpp
+++ wonchang/기초/기초2_2/기초2_2_Brute Force/1248/1248/main.cpp
@@ -7,17 +7,52 @@
 
 #include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
 int n = 0;
-int main(int argc, const char * argv[]) {
-    string str = "";
+int arr[10] = {0, };
+char sign[10][10];
+bool flag = false;
+bool check(int cnt){
+    int sum = 0;
+    for(int i = cnt; i >= 0; i--){
+        sum += arr[i];
+        if(sign[i][cnt] == '+' && sum <= 0)
+            return false;
+        else if(sign[i][cnt] == '0' && sum != 0)
+            return false;
+        else if(sign[i][cnt] == '-' && sum >= 0)
+            return false;
+    }
     
+    return true;
+}
+void go(int cnt){
+    if(flag)
+        return;
+    if(cnt == n){
+        for(int i = 0; i < n; i++){
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        flag = true;
+        
+        return;
+    }
+    for(int i = -10; i < 11; i++){
+        arr[cnt] = i;
+        if(check(cnt))
+            go(cnt + 1);
+    }
+}
+int main(int argc, const char * argv[]) {
     cin >> n;
     getchar();
-    cin >> str;
-    
-    
-    
-    
+    for(int i = 0; i < n; i++){
+        for(int j = i; j < n; j++){
+            cin >> sign[i][j];
+        }
+    }
+    go(0);
     return 0;
 }
