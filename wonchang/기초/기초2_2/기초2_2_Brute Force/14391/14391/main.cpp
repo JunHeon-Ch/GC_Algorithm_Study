82c9cad7a757ec57ca2176fb0651922b6b3abf7f
BlackChang <dnjs7651@gmail.com>
2021-01-25 17:55:41 +0900

    Success

--- wonchang/기초/기초2_2/기초2_2_Brute Force/14391/14391/main.cpp
+++ wonchang/기초/기초2_2/기초2_2_Brute Force/14391/14391/main.cpp
@@ -10,7 +10,56 @@
 using namespace std;
 int n = 0;
 int m = 0;
+int ans = 0;
 int arr[4][4];
+bool visit[4][4];
+void go(int cnt, int sum){
+    int r = cnt % m;
+    int c = cnt / m;
+    if(cnt >= n * m){
+        ans = max(ans, sum);
+        return;
+    }
+    if(visit[c][r])
+        go(cnt + 1, sum);
+    else{
+        int tmp = 0;
+        tmp = arr[c][r];
+        visit[c][r] = true;
+        go(cnt + 1, sum + tmp);
+        visit[c][r] = false;
+        for(int i = 1; i < n - c; i++){
+            int x = r;
+            int y = c + i;
+            tmp *= 10;
+            tmp += arr[y][x];
+            for(int j = 1; j <= i; j++){
+                visit[c + j][x] = true;
+            }
+            go(cnt + 1, sum + tmp);
+            for(int j = 1; j <= i; j++){
+                visit[c + j][x] = false;
+            }
+        }
+        tmp = arr[c][r];
+        
+        for (int i = 1; i < m - r; i++) {
+            int x = r + i;
+            int y = c;
+            if (visit[y][x])
+                return;
+            tmp *= 10;
+            tmp += arr[y][x];
+            for(int j = 1; j <= i; j++){
+                visit[y][r + j] = true;
+            }
+            go(cnt + 1, sum + tmp);
+            for (int j = 1; j <= i; j++){
+                visit[y][r + j] = false;
+            }
+        }
+    }
+}
 int main(int argc, const char * argv[]) {
     cin >> n >> m;
     for(int i = 0; i < n; i++){
@@ -21,7 +70,8 @@ int main(int argc, const char * argv[]) {
         }
     }
     
-    
-
+    go(0, 0);
+    cout << ans << '\n';
     return 0;
 }
+
