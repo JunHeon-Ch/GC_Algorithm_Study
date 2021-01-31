d54b54545b05fb6d6b651a416f25d03d9a66ab27
JunHeon-Ch <chjh121@gmail.com>
2021-01-31 09:46:48 +0900

    16964 DFS 스페셜 저지

---
+++ junheon/기초/#5_그래프/#13_16964 DFS 스페셜 저지/16964.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+vector<int> g[100000];
+bool c[100000];
+int n;
+vector<int> ans;
+
+void dfs(int s) {
+    if(c[s]) {
+        return;
+    }
+    c[s] = true;
+    ans.push_back(s);
+    for(int n : g[s]) {
+        if(!c[n]) {
+            dfs(n); 
+        }
+    }
+}
+
+int main() {
+    cin >> n;
+    for(int i = 0; i < n - 1; i++) {
+        int u, v;
+        cin >> u >> v;
+        u--; v--;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+    vector<int> b(n);
+    vector<int> o(n);
+    for(int i = 0; i < n; i++) {
+        cin >> b[i];
+        b[i]--;
+        o[b[i]] = i;
+    }
+    for(int i = 0; i < n; i++) {
+        sort(g[i].begin(), g[i].end(), [&](const int &u, const int &v) {
+            return o[u] < o[v];
+        });
+    }
+
+    dfs(0);
+    if(b == ans) {
+        cout << 1 << '\n';
+    }
+    else {
+        cout << 0 << '\n';
+    }
+
+    return 0;
+}
