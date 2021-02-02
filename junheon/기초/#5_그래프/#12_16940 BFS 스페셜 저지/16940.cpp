58c1221bd6d4081357d938d5a21136a0b2db0e62
JunHeon-Ch <chjh121@gmail.com>
2021-02-02 15:28:21 +0900

    16940 BFS 스페셜 저지

--- junheon/기초/#5_그래프/#12_16940 BFS 스페셜 저지/16940.cpp
+++ junheon/기초/#5_그래프/#12_16940 BFS 스페셜 저지/16940.cpp
@@ -36,16 +36,19 @@ int main() {
     p[0] = 0;
     int k = 1;
     for(int i = 0; i < n; i++) {
+        // 모든 정점을 다 방문하기 전에 큐가 비어 있으면 틀림 
         if(q.empty()) {
             cout << 0 << '\n';
             return 0;
         }
         int c = q.front();
         q.pop();
+        // 큐에 저장된 순서랑 입력된 순서랑 다르면 틀림
         if(c != o[i]) {
             cout << 0 << '\n';
             return 0;
         }
+        // 정점의 차수를 cnt에 저장
         int cnt = 0;
         for(int n : g[c]) {
             if(p[n] == -1) {
@@ -53,7 +56,10 @@ int main() {
                 cnt++;
             }
         }
+        // 정점의 차수만큼 입력된 순서만큼 큐에 넣음
         for(int j = 0; j < cnt; j++) {
+            // 입력된 순서의 부모 정점이 부모 정점이랑 다르거나
+            // 큐에 입력된 수가 정점의 수보다 크면 틀림
             if(p[o[k + j]] != c || k + j >= n) {
                 cout << 0 << '\n';
                 return 0;
