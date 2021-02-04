8bfe861cc28547917e38e88213f8605b898b3db1
BlackChang <dnjs7651@gmail.com>
2021-02-04 15:19:54 +0900

    Success

--- wonchang/coding test/기초/1037/1037/main.cpp
+++ wonchang/coding test/기초/1037/1037/main.cpp
@@ -6,9 +6,22 @@
 //
 
 #include <iostream>
-
+#include <vector>
+#include <algorithm>
+using namespace std;
 int main(int argc, const char * argv[]) {
+    int n = 0;
+    vector<int> v;
     
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        int tmp = 0;
+        
+        cin >> tmp;
+        v.push_back(tmp);
+    }
     
+    sort(v.begin(), v.end());
+    cout << v.front() * v.back() << '\n';
     return 0;
 }
