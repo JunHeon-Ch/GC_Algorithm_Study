11d5a5fa9e1ed269c8c6c42ef328c0387b542c8d
BlackChang <dnjs7651@gmail.com>
2021-02-04 15:59:18 +0900

    Success

---
+++ wonchang/coding test/기초/18290/18290/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  18290
+//
+//  Created by 이원창 on 2021/02/04.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    int n = 0;
+    int m = 0;
+    int k = 0;
+    
+    cin >> n >> m >> k;
+    
+    
+    return 0;
+}
