392ba89c8a82b5846de16b9e28630c26eccb42f6
JunHeon-Ch <chjh121@gmail.com>
2021-02-07 10:58:16 +0900

    18290 NM과 K(1)

--- junheon/코딩테스트/기초/브루트포스/#1_18290 NM과 K(1)/18290.cpp
+++ junheon/코딩테스트/기초/브루트포스/#1_18290 NM과 K(1)/18290.cpp
@@ -43,6 +43,9 @@ void go(int cnt, int sum) {
 }
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
     cin >> n >> m >> k;
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
