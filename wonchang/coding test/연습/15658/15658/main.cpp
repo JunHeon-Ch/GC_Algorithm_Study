ac7a5a5c00d0eca9278d3307d09e858b97fd0ab2
BlackChang <dnjs7651@gmail.com>
2021-02-11 21:22:28 +0900

    Success

---
+++ wonchang/coding test/연습/15658/15658/main.cpp
@@ -0,0 +1,60 @@
+//
+//  main.cpp
+//  15658
+//
+//  Created by 이원창 on 2021/02/11.
+//
+
+#include <iostream>
+#include <climits>
+using namespace std;
+int n = 0;
+int op[4];
+long long arr[100];
+long long maxAns = LONG_LONG_MIN;
+long long minAns = LONG_LONG_MAX;
+long long cal(int idx, int num, int op){
+    long long tmp = 0;
+    if(op == 0)
+        tmp = num + arr[idx + 1];
+    else if(op == 1)
+        tmp = num - arr[idx + 1];
+    else if(op == 2)
+        tmp = num * arr[idx + 1];
+    else
+        tmp = num / arr[idx + 1];
+    
+    return tmp;
+}
+void go(int cnt, long long num){
+    if(cnt == n - 1){
+        maxAns = max(maxAns, num);
+        minAns = min(minAns, num);
+        return;
+    }
+    
+    for(int i = 0; i < 4; i++){
+        if(op[i] != 0){
+            op[i]--;
+            go(cnt + 1, cal(cnt, num, i));
+            op[i]++;
+        }
+    }
+}
+int main(int argc, const char * argv[]) {
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    
+    for(int i = 0; i < 4; i++){
+        cin >> op[i];
+    }
+    
+    go(0, arr[0]);
+    cout << maxAns << '\n';
+    cout << minAns << '\n';
+    return 0;
+}
+
+
