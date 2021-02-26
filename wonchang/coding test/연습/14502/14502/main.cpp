e1cd7d4123a53e7b13872d598d4c12951d215db1
BlackChang <dnjs7651@gmail.com>
2021-02-26 20:32:55 +0900

    New Project

--- wonchang/coding test/연습/14502/14502/main.cpp
+++ wonchang/coding test/연습/14502/14502/main.cpp
@@ -6,9 +6,19 @@
 //
 
 #include <iostream>
-
+using namespace std;
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int n = 0;
+    int m = 0;
+    int arr[8][8];
+    
+    cin >> n >> m;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            cin >> arr[i][j];
+        }
+    }
+    
+    
     return 0;
 }
