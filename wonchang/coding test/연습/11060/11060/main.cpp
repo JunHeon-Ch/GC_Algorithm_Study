2bf9492d474399fb00dac39ef9738c16fdb750d6
BlackChang <dnjs7651@gmail.com>
2021-03-14 17:59:16 +0900

    Success

--- wonchang/coding test/연습/11060/11060/main.cpp
+++ wonchang/coding test/연습/11060/11060/main.cpp
@@ -6,6 +6,7 @@
 //
 
 #include <iostream>
+#include <cstring>
 using namespace std;
 int n = 0;
 int arr[1000];
@@ -13,21 +14,19 @@ int dp[1000];
 int main(int argc, const char * argv[]) {
     cin >> n;
     
+    memset(dp, -1, sizeof(dp));
     for(int i = 0; i < n; i++){
         cin >> arr[i];
-        dp[i] = 1001;
     }
     
     dp[0] = 0;
     for(int i = 1; i < n; i++){
-        for(int j = 0; j < arr[i]; j++){
-            dp[i + j] = min(dp[i] + 1, dp[i + j]);
+        for(int j = i - 1; j >= 0; j--){
+            if(arr[j] + j >= i && dp[j] != -1)
+                dp[i] = dp[j] + 1;
         }
     }
     
-    for(int i = 0; i < n; i++){
-        cout << dp[i] << ' ';
-    }
-//    cout << dp[n - 1] << '\n';
+    cout << dp[n - 1] << '\n';
     return 0;
 }
