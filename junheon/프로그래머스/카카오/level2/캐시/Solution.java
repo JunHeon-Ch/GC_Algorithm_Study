8b890ee7e5a8318e4edaf42146b725e7e4eb1054
JunHeon-Ch <chjh121@gmail.com>
2021-06-23 14:37:26 +0900

    level 2: 캐시

---
+++ junheon/프로그래머스/카카오/level2/캐시/Solution.java
@@ -0,0 +1,21 @@
+package 프로그래머스.카카오.level2.캐시;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution {
+    public int solution(int cacheSize, String[] cities) {
+        Queue<String> queue = new LinkedList<>();
+        int t = 0;
+        for(String city : cities) {
+            city = city.toLowerCase();
+            if(queue.contains(city)) t++;
+            else {
+                t += 5;
+                queue.add(city);
+                if(queue.size() > cacheSize) queue.poll();
+            }
+        }
+        return t;
+    }
+}
