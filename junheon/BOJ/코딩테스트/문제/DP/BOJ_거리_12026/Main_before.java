c37ef5b673d697d93c833565e2973ab678c206bc
JunHeon-Ch <chjh121@gmail.com>
2021-07-04 22:17:09 +0900

    12026 BOJ 거리

--- junheon/BOJ/코딩테스트/문제/DP/BOJ_거리_12026/Main_before.java
+++ junheon/BOJ/코딩테스트/문제/DP/BOJ_거리_12026/Main_before.java
@@ -11,7 +11,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class Main_before {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
