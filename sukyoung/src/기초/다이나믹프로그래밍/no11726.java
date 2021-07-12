46a23e14ec366677c16f5b14748a2ffa7829d8fe
JeongSuKyoung <jsk_1025@naver.com>
2021-07-12 23:09:27 +0900

    2xn 타일링

---
+++ sukyoung/src/기초/다이나믹프로그래밍/no11726.java
@@ -0,0 +1,22 @@
+package 기초.다이나믹프로그래밍;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class no11726 {
+    public static Integer[] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        dp = new Integer[n+1];
+        dp[0] = dp[1] = 1;
+
+        if(n>=2){
+            for(int i=2; i<=n; i++){
+                dp[i] = (dp[i-1] + dp[i-2]) % 10007;
+            }
+        }
+        System.out.println(dp[n]);
+    }
+}
