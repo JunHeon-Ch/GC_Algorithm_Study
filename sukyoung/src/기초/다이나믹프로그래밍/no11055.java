032704c23dc86f8bbe18b0946913e2b04271d650
JeongSuKyoung <jsk_1025@naver.com>
2021-07-21 22:02:29 +0900

    가장 큰 증가 부분 수열

---
+++ sukyoung/src/기초/다이나믹프로그래밍/no11055.java
@@ -0,0 +1,32 @@
+package 기초.다이나믹프로그래밍;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class no11055 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        int[] dp = new int[n];
+        dp[0] = arr[0];
+        for(int i=1; i<n; i++){
+            dp[i] = arr[i];
+            for(int j=0; j<i; j++){
+                if(arr[j] < arr[i]){
+                    dp[i] = Math.max(dp[j]+arr[i], dp[i]);
+                }
+            }
+        }
+        Arrays.sort(dp);
+        System.out.println(dp[n-1]);
+    }
+}
