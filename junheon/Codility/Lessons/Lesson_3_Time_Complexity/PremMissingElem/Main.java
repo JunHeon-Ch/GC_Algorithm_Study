3ef78602a3bd4b32d7acf5cb066c0e92dc826f9a
JunHeon-Ch <chjh121@gmail.com>
2021-08-04 13:22:39 +0900

    Lesson 3: Prem Missing Elem

---
+++ junheon/Codility/Lessons/Lesson_3_Time_Complexity/PremMissingElem/Main.java
@@ -0,0 +1,19 @@
+package Codility.Lessons.Lesson_3_Time_Complexity.PremMissingElem;
+
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] A) {
+        int n = A.length;
+        Arrays.sort(A);
+        int ans = 0;
+        for(int i = 0; i < n; i++) {
+            if(A[i] != i + 1) {
+                ans = i + 1;
+                break;
+            }
+        }
+        if(ans == 0) ans = n + 1;
+        return ans;
+    }
+}
