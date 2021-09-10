7393fb7653808b38afe7adefcbda2f0c73a15b43
JunHeon-Ch <chjh121@gmail.com>
2021-09-10 20:32:58 +0900

    Level 2: 영어 끝말잇기

---
+++ junheon/프로그래머스/level2/영어_끝말잇기/Solution.java
@@ -0,0 +1,31 @@
+package 프로그래머스.level2.영어_끝말잇기;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, String[] words) {
+        List<String> list = new ArrayList<>();
+        int cnt = 0;
+        String prev = "";
+        for(String now : words) {
+            if(list.contains(now)) break;
+            if(!prev.equals("") && prev.charAt(prev.length() - 1) != now.charAt(0)) break;
+            cnt++;
+            list.add(now);
+            prev = now;
+        }
+        int[] ans = new int[2];
+        if(cnt == words.length) {
+            ans[0] = 0;
+            ans[1] = 0;
+        } else {
+            cnt++;
+            ans[0] = cnt % n == 0 ? n : cnt % n;
+            ans[1] = (int)Math.ceil((double) cnt / n);
+        }
+        return ans;
+    }
+}
