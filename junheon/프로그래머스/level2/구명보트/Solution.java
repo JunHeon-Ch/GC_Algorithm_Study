e17cc88e4cf6ece103fa56f44a7f726ea6a65602
JunHeon-Ch <chjh121@gmail.com>
2021-09-10 22:16:01 +0900

    Level 2: 구명보트

---
+++ junheon/프로그래머스/level2/구명보트/Solution.java
@@ -0,0 +1,20 @@
+package 프로그래머스.level2.구명보트;
+
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        int n = people.length;
+        int max = n - 1;
+        int min = 0;
+        int ans = 0;
+        while(min <= max) {
+            if(people[min] + people[max] <= limit)
+                min++;
+            max--;
+            ans++;
+        }
+        return ans;
+    }
+}
