c667a722d9ca2c0b01f9d8a7b62e2b5e4e3c7434
JunHeon-Ch <chjh121@gmail.com>
2022-01-18 16:46:24 +0900

    14567 선수과목

---
+++ junheon/BOJ/위상정렬/선수과목_14567/Before_Main.java
@@ -0,0 +1,60 @@
+package BOJ.위상정렬.선수과목_14567;
+
+import java.io.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+/*
+https://www.acmicpc.net/problem/14567
+ * 알고리즘: 위상정렬
+ * 시간복잡도:
+ */
+
+public class Before_Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        boolean[][] dag = new boolean[n][n];
+        for(int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            dag[--u][--v] = true;
+        }
+
+        int cnt = 0, res = 1;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int[] ans = new int[n];
+        while(cnt < n) {
+            Stack<Integer> stack = new Stack<>();
+            for(int j = 0; j < n; j++) {
+                if(ans[j] > 0) continue;
+                int i;
+                for(i = 0; i < n; i++) {
+                    if(dag[i][j]) break;
+                }
+                if(i == n) {
+                    stack.push(j);
+                }
+            }
+            cnt += stack.size();
+
+            while(!stack.isEmpty()) {
+                int i = stack.pop();
+                ans[i] = res;
+                for(int j = 0; j < n; j++) {
+                    dag[i][j] = false;
+                }
+            }
+            res++;
+        }
+
+        for(int i = 0; i < n; i++) {
+            bw.write(ans[i] + " ");
+        }
+        bw.flush();
+    }
+}
