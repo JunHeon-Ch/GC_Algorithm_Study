c30f34b791df9987c524eac01fa76538c2b218e9
JunHeon-Ch <chjh121@gmail.com>
2022-08-30 19:42:14 +0900

    ok

--- junheon/PS_2021/BOJ/이분탐색/공유기_설치_2110/Main.java
+++ junheon/PS_2021/BOJ/이분탐색/공유기_설치_2110/Main.java
@@ -14,12 +14,12 @@ https://www.acmicpc.net/problem/2110
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
-        int[] house = new int[n];
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            int[] house = new int[n];
         for(int i = 0; i < n; i++) {
             house[i] = Integer.parseInt(br.readLine());
         }
